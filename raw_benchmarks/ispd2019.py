27761a50882cae8755cb04fa73d8dffe80669630
GradInnovate <magi+github@gradinnovate.com>
2026-04-26 18:12:08 +0800

    fix: batch fix for issues #1-#5, #7-#11 (#12)
    Fixes:
    - #1: Update README clone URL to gradinnovate-agents
    - #2: Rename requiremens.txt -> requirements.txt
    - #3: Add Boost prerequisites to README
    - #4: Add missing RowMap.cpp and SubRowNodeMap.cpp to CMakeLists
    - #5: Replace pip freeze dump with curated requirements.txt
    - #7: Update CI (cibuildwheel v2.23.3, drop cp38, OIDC publish)
    - #8: Remove .gitmodules from .gitignore
    - #9: Add filter='data' to tar.extractall calls
    - #10: Remove dead Python 2 compat code, fix bare except
    - #11: Add dependencies, urls, optional-deps to pyproject.toml
    
    Note: Issue #6 (test assertions) confirmed but requires benchmark
    data to add meaningful assertions.
    
    Co-authored-by: Irene Agent <irene@gradinnovate.com>

--- raw_benchmarks/ispd2019.py
+++ raw_benchmarks/ispd2019.py
@@ -1,12 +1,7 @@
 import os
 import sys
 import tarfile
-if sys.version_info[0] < 3:
-    import urllib2 as urllib
-    from StringIO import StringIO
-else:
-    import urllib.request as urllib
-    from io import BytesIO as StringIO
+import urllib.request as urllib
 
 baseURL = "http://www.ispd.cc/contests/19/benchmarks/"
 target_dir = os.path.dirname(os.path.abspath(__file__))
@@ -14,7 +9,7 @@ filenames = [f"ispd19_test{i}.tgz" for i in range(1, 11)]
 ispd19_dir = os.path.join(target_dir, "ispd2019")
 try:
     os.mkdir(ispd19_dir)
-except:
+except OSError:
     pass
 
 for filename in filenames:
@@ -29,7 +24,7 @@ for filename in filenames:
 
     print("Uncompress %s to %s" % (path_to_file, ispd19_dir))
     with tarfile.open(path_to_file, 'r:gz') as tar:
-        tar.extractall(ispd19_dir)
+        tar.extractall(ispd19_dir, filter='data')
 
     print("remove downloaded file %s" % (path_to_file))
     os.remove(path_to_file)
